ad033c97d5f4bdc32e5e3656a011fe36262c45cc
Fredrick Lambuth <flambuth@gmail.com>
2019-10-29 15:51:40 -0500

    the vandals rock

--- model.py
+++ model.py
@@ -1,7 +1,7 @@
 from sklearn.tree import DecisionTreeClassifier
 
-def do_the_decisionTree(my_criterion, X_train, y_train):
-    clf = DecisionTreeClassifier(criterion=my_criterion, max_depth=3, random_state=123)
+def do_the_decisionTree(my_criterion, X_train, y_train, max_depth=3):
+    clf = DecisionTreeClassifier(criterion=my_criterion, max_depth=max_depth, random_state=123)
     clf.fit(X_train, y_train)
     y_pred = clf.predict(X_train)
     y_pred_proba = clf.predict_proba(X_train)
